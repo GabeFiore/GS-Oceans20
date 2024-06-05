50d6870ebc97679160882161417c9e91a17ee48a
Gabriel Fiore <gabriel_fiore@hotmail.com>
2024-06-05 00:23:27 -0300

    home finalizada

---
+++ src/Components/Carrosel/Carrosel.jsx
@@ -0,0 +1,121 @@
+import { useState, useEffect } from "react";
+
+const Carrosel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    "../src/assets/corais.jpg",
+    "../src/assets/tartaruga.jpg",
+    "../src/assets/peixepalhaco.jpg",
+  ];
+
+  const Previous = () => {
+    const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const Next = () => {
+    const newIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const Slide = (index) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      Next();
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  return (
+    <div className="relative w-full h-full" data-carousel="slide">
+      <div className="relative w-full h-full overflow-hidden rounded-lg">
+        {images.map((src, index) => (
+          <div
+            key={index}
+            className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${
+              index === currentIndex ? "opacity-100" : "opacity-0"
+            }`}
+            data-carousel-item={index === currentIndex}
+          >
+            <img
+              src={src}
+              className="absolute w-full h-full object-cover"
+              alt={`Slide ${index + 1}`}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`w-3 h-3 rounded-full ${
+              index === currentIndex ? "bg-white" : "bg-gray-300"
+            }`}
+            aria-current={index === currentIndex}
+            aria-label={`Slide ${index + 1}`}
+            data-carousel-slide-to={index}
+            onClick={() => Slide(index)}
+          />
+        ))}
+      </div>
+      <button
+        type="button"
+        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-prev
+        onClick={Previous}
+      >
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+          <span className="sr-only">Previous</span>
+        </span>
+      </button>
+      <button
+        type="button"
+        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-next
+        onClick={Next}
+      >
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+          <span className="sr-only">Next</span>
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default Carrosel;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../styles/pages/_home.scss";
+import Carrosel from "../../Components/Carrosel/Carrosel";
 
 function Home() {
   return (
     <>
       <div className="home">
-        <h2 className="home-title">Bem-vindo à plataforma sobre oceanos</h2>
+        <div className="container">
+          <div className="intro">
+            <h2 className="title">Bem-vindo à plataforma OceanGuard!</h2>
+            <p>
+              Nossa missão é promover a conservação dos oceanos através da
+              conscientização, educação e ações sustentáveis. Junte-se a nós na
+              luta para proteger nossos preciosos recursos marinhos e garantir
+              um futuro saudável para as próximas gerações e para o planeta
+              Terra.
+            </p>
+          </div>
+          <div className="carrosel">
+            <Carrosel />
+          </div>
+        </div>
+        <article className="info">
+          <h2>Entenda um pouco mais sobre a situação dos nossos oceanos:</h2>
+          <div className="overview">
+            <img
+              src="../src/assets/plastico-oceano.jpg"
+              alt="Poluição nos oceanos"
+            />
+            <div className="texto">
+              <div className="paragrafos">
+                <p>
+                  Os oceanos enfrentam diversos desafios críticos, como a
+                  destruição de habitats, poluição e mudanças climáticas. A
+                  conservação e sustentabilidade dos oceanos são essenciais para
+                  manter a saúde dos ecossistemas marinhos e garantir a
+                  sobrevivência das espécies que dependem deles, incluindo os
+                  seres humanos.
+                </p>
+                <p>
+                  A poluição marinha, proveniente de resíduos plásticos,
+                  produtos químicos e derramamentos de óleo, afeta a qualidade
+                  da água e ameaça a vida marinha. As mudanças climáticas estão
+                  alterando as temperaturas dos oceanos, afetando os padrões de
+                  migração e reprodução das espécies marinhas.
+                </p>
+                <p>
+                  Para descobrir como você pode fazer a diferença e ajudar na
+                  conservação dos oceanos: <Link to="geral">Clique Aqui!</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </article>
       </div>
     </>
   );
